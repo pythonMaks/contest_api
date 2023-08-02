26897d4c2c4f8c364efd8d0df20886fbab43cce4
pythonMaks <python.maxim@yandex.ru>
2023-08-02 14:36:54 +0300

    t

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 from .views import TaskListCreate, TestListCreate, TaskDetail, TestDetail, SubmissionCreateView, SubmissionListView, StudentSubmissionListView
 
-urlpatterns = [
-    path('tasks/', TaskListCreate.as_view(), name='task-list-create'),
-    path('tests/', TestListCreate.as_view(), name='test-list-create'),
-    path('tasks/<int:pk>/', TaskDetail.as_view(), name='task-detail'),
-    path('tests/<int:pk>/', TestDetail.as_view(), name='test-detail'),
+urlpatterns = [    
     path('submission/', SubmissionCreateView.as_view(), name='submission-create'),
     path('submissions/', SubmissionListView.as_view(), name='submission-list'),
     path('submissions/<str:uid>/', StudentSubmissionListView.as_view(), name='student-submission-list'),
