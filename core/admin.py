7e788c64884e1f68881efbb1f5d8177711bb14c5
pythonMaks <python.maxim@yandex.ru>
2023-08-02 12:01:10 +0300

    t

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Submission, Task, Test
 
+admin.site.register(Submission)
+admin.site.register(Task)
+admin.site.register(Test)
 # Register your models here.
